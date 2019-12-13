32a5576916248f45785218240921da9e2f7d3c3c
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2019-12-13 12:24:10 -0600

    seed data created

--- data/seeds/addTasks.js
+++ data/seeds/addTasks.js
@@ -1,10 +1,10 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('tasks').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('tasks').insert([
         { id: 1, task_description: 'answer all questions', task_notes: 'reference tk', project_id: 1, completed: 0 },
         { id: 2, task_description: 'call or email', task_notes: 'reference website', project_id: 2, completed: 0 },
         { id: 3, task_description: ' this old house', task_notes: 'watch tv show', project_id: 3, completed: 0 }
