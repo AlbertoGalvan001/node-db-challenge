a67cc185e9ebfc1c44df2c4f61d1406ee27b092f
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2019-12-15 10:31:52 -0600

    server modifications, server is running

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 
 const ProjectsRouter = require('./projects/projects-router');
-const ResourcesRouter = require('./resources/resources-router');
+// const ResourcesRouter = require('./resources/resources-router');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use('/api/projects', ProjectsRouter);
-server.use('/api/resources', ResourcesRouter);
+
 
 
 //initial GET
