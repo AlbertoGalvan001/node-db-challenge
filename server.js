e4302757f25b1897ef2dbd1419011db406676e2b
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2019-12-15 13:20:18 -0600

    Tasks model/routers completed, find and add functions are working

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const helmet = require('helmet');
 
 const ProjectsRouter = require('./projects/projects-router');
 const ResourcesRouter = require('./resources/resources-router');
+const TasksRouter = require('./tasks/tasks-router.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
 server.use('/api/projects', ProjectsRouter);
 server.use('/api/resources', ResourcesRouter);
+server.use('/api/tasks', TasksRouter);
 
 
 
